655309d1e2717d563b8c08db8a9ee9fe78d281b4
JFT70 <94490112+JFT70@users.noreply.github.com>
2024-05-07 14:01:22 -0400

    Added treesitter-textobjects and many keybindings to use it

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,47 @@
 return
 {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   opts = {
     ensure_installed = {
-        -- default
-        -- "vim", "lua", "vimdoc",
-        -- "html", "css"
-        "c",
-        "css",
-        "cpp",
-        "cmake",
-        "css",
-        "dockerfile",
-        "elixir",
-        "go",
-        "haskell",
-        "html",
-        "javascript",
-        "json",
-        -- "lua",
-        "markdown",
-        "markdown_inline",
-        "ocaml",
-        "python",
-        "rust",
-        "toml",
-        "tsx",
-        "typescript",
-        -- "vim",
-        -- "vimdoc",
+      -- default
+      -- "vim", "lua", "vimdoc",
+      -- "html", "css"
+      "c",
+      "css",
+      "cpp",
+      "cmake",
+      "css",
+      "dockerfile",
+      "elixir",
+      "go",
+      "haskell",
+      "html",
+      "javascript",
+      "json",
+      -- "lua",
+      "markdown",
+      "markdown_inline",
+      "ocaml",
+      "python",
+      "rust",
+      "toml",
+      "tsx",
+      "typescript",
+      -- "vim",
+      -- "vimdoc",
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      }
+    }
   },
 }
