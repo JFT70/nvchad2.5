09ece7eb7e0ae76486abe97c16c4636bda939319
JFT70 <94490112+JFT70@users.noreply.github.com>
2025-08-28 11:02:11 -0400

    Have both abs and rel line numbers

--- lua/options.lua
+++ lua/options.lua
@@ -14,7 +14,45 @@ o.wrap=false
 -- See column 80
 --opt.colorcolumn = "80"
 
+o.number = true
 o.relativenumber = true
+
+-- BEGIN sgtpeper status column
+vim.api.nvim_set_hl(0, 'StatusColumnLineNr', { fg = '#585b70', bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'StatusColumnLineNrCursor', { fg = '#f9e2af', bg = 'Black', reverse = true })
+
+vim.o.statuscolumn = table.concat {
+  '%@SignCb@', -- Clickable sign column
+  '%s', -- Sign column
+  '%=', -- Right align
+
+  -- Absolute line number (current line)
+  '%#StatusColumnLineNrCursor#',
+  "%{v:virtnum == 0 ? (v:lnum == line('.') ? v:lnum : '') : ''}",
+
+  -- Absolute line number (non-current line)
+  '%#StatusColumnLineNr#',
+  "%{v:virtnum == 0 ? (v:lnum == line('.') ? '' : v:lnum) : ''}",
+
+  -- Padding space (current line)
+  '%#StatusColumnLineNrCursor#',
+  "%{v:virtnum == 0 ? (v:relnum == 0 ? ' ' : '') : ''}",
+
+  -- Padding space (non-current line)
+  '%#StatusColumnLineNr#',
+  "%{v:virtnum == 0 ? (v:relnum == 0 ? '' : ' ') : ''}",
+
+  -- Relative line number (current line)
+  '%#StatusColumnLineNrCursor#',
+  "%-2{v:virtnum == 0 ? (v:relnum == 0 ? v:relnum : '') : ''}",
+
+  -- Relative line number (non-current line)
+  '%#StatusColumnLineNr#',
+  "%-2{v:virtnum == 0 ? (v:relnum == 0 ? '' : v:relnum) : ''}",
+
+  '│ ', -- Separator
+}
+-- END sgtpeper status column
 
 o.termguicolors = true
 
