0823fa75bf058bf4c69e2f642b39e42d6d1066c6
JFT70 <94490112+JFT70@users.noreply.github.com>
2025-08-28 11:05:49 -0400

    Fixed Python DAP

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -5,9 +5,4 @@ return {
     "mfussenegger/nvim-dap",
     "rcarriga/nvim-dap-ui",
   },
-  config = function (_, opts)
-   local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-    require("dap-python").setup(path)
-    require("core.utils").load_mappings("dap_python")
-  end,
 }
